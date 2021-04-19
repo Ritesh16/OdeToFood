0cede3be8ee6c598ec8e65effbb122a8eca9482c
Ritesh16 <ritesh.sharma.cse@gmail.com>
2021-04-19 15:49:33 -0400

    Adding restaurant page

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,37 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantByName(string name);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public List<Restaurant> Restaurants { get; set; }
+
+        public InMemoryRestaurantData()
+        {
+            Restaurants = new List<Restaurant>
+            {
+                new Restaurant {Id=1, Name="Grotto's Pizza", Location="Middletown", Cusine = CusineType.Italian},
+                new Restaurant {Id=2, Name="Maharaja", Location="Newark", Cusine = CusineType.Indian},
+                new Restaurant {Id=3, Name="Qdoba", Location="Baltimore", Cusine = CusineType.Mexican},
+                new Restaurant {Id=4, Name="Sam's Pizza", Location="Middletown", Cusine = CusineType.Italian}
+
+            };
+        }
+        
+        public IEnumerable<Restaurant> GetRestaurantByName(string name)
+        {
+            return from r in this.Restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
-        public void OnGet()
+        private readonly IConfiguration configuration;
+        private readonly IRestaurantData restaurantData;
+
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public ListModel(IConfiguration configuration, IRestaurantData restaurantData)
         {
+            this.configuration = configuration;
+            this.restaurantData = restaurantData;
+        }
+        public void OnGet(string searchTerm)
+        {
+            Restaurants = restaurantData.GetRestaurantByName(searchTerm);
         }
     }
 }
